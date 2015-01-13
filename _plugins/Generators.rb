e3a3cb44d5b9e28a81f9ec987d31e2bc82e529b7
David Tyler <dave@demonic.org.uk>
2015-01-13 18:12:31 +0000

    present lifelist additions on posts

--- _plugins/Generators.rb
+++ _plugins/Generators.rb
@@ -4,9 +4,12 @@ module Jekyll
       lifelist = Array.new
       site.posts.each do | post |
         if post.data['birds']
+          post.data['lifelist'] = []
           for bird in post.data['birds']
-            # sanitise case here
-            lifelist.push(bird.downcase) unless lifelist.include? bird.downcase
+            # Using capitalize here to prevent dupes.
+            # Should maybe revamp this storing a hash of arrays with additional data?
+            post.data['lifelist'].push(bird) unless lifelist.include? bird.capitalize # add to lifelist array on post
+            lifelist.push(bird.capitalize) unless lifelist.include? bird.capitalize # add to lifelist for page.
           end
         end
       end
